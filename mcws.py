05a43af392a7a48c82422ebf23ce033d6a81a45e
haveyouwantto <haveyouwantto@gmail.com>
2019-12-06 02:11:47 +0800

    fix midi player

--- mcws.py
+++ mcws.py
@@ -1,10 +1,13 @@
 import asyncio
-import websockets
-import json
-import time
-import mido
-import os
 import glob
+import json
+import os
+import threading
+import time
+import sys
+
+import mido
+import websockets
 
 
 def cmd(line):
@@ -38,11 +41,13 @@ sub = json.dumps({
 })
 
 helpmsg = {
-    '.test': '测试用命令    \u00a7a.test',
-    '.help': '提供帮助/命令列表    \u00a7a.help',
-    '.function': '运行在相应的功能文件中找到的命令    \u00a7a.function <function>',
-    '.midi': '播放一个mid文件    \u00a7a.midi <file>'
+    '.test': '测试用命令    \u00a7c.test',
+    '.help': '提供帮助/命令列表    \u00a7c.help',
+    '.function': '运行在相应的功能文件中找到的命令    \u00a7c.function <function>',
+    '.midi': '播放一个mid文件    \u00a7c.midi <file>'
 }
+
+play = True
 
 
 def getChat(msg):
@@ -50,27 +55,73 @@ def getChat(msg):
 
 
 def info(msg):
-    return cmd("say \u00a7d"+str(msg))
+    return cmd("say \u00a7d" + str(msg))
 
 
 def play_note(midimsg):
+    origin = midimsg.note - 66
+    pitch = 2 ** (origin / 12)
+    volume = midimsg.velocity / 128
+    return cmd("execute @a ~ ~ ~ playsound note.harp @s ^0 ^ ^ " + str(volume) + " " + str(pitch))
 
-    origin = midimsg.note-66
-    pitch = 2**(origin/12)
-    volume = midimsg.velocity/128
-    if midimsg.channel == 10:
-        return cmd("")
-    return cmd("playsound note.harp @a ^0 ^ ^ "+str(volume)+" "+str(pitch))
 
+def runmain(coroutine):
+    try:
+        coroutine.send(None)
+    except StopIteration as e:
+        return e.value
+
+
+class midiplayer(threading.Thread):
+    def __init__(self, ws):
+        threading.Thread.__init__(self)
+        self.ws = ws
+        self.playing = False
+        self.mid = None
+        self.setName('Midi Player Thread')
+
+    def run(self):
+        while True:
+            if self.playing:
+                for msg in self.mid.play():
+                    if msg.type == "note_on":
+                        print(msg)
+                        runmain(self.ws.send(play_note(msg)))
+                    if not self.playing:
+                        break
+
+    def set_midi(self, mid):
+        self.mid = mido.MidiFile(mid)
+
+    def play(self):
+        self.playing = True
+
+    def stop(self):
+        self.playing = False
 
 
 def setBlock(x, y, z, id, data=0):
-    return cmd("setblock "+str(x)+" "+str(y)+" "+str(z)+" "+str(id)+" "+str(data))
+    return cmd("setblock " + str(x) + " " + str(y) + " " + str(z) + " " + str(id) + " " + str(data))
+
+
+def miidDisplay():
+    out = '/titleraw @s actionbar {"rawtext":[{"text":"'
+    for i in range(128):
+        out += "\u258F"
+    return out+'"}]}'
 
 
 async def hello(ws, path):
+
+    player = midiplayer(ws)
+    player.start()
+
     await ws.send(sub)
+
     sender = "外部"
+
+    log = open('log.txt', 'a')
+
     while True:
         data = await ws.recv()
         msg = json.loads(data)
@@ -79,15 +130,22 @@ async def hello(ws, path):
 
             if msg["body"]["eventName"] == "PlayerMessage" and msg["body"]["properties"]["Sender"] != sender:
 
-                raw=getChat(msg)
+                raw = getChat(msg)
+
+                log.write(raw+"\n")
+
                 args = raw.split(" ")
 
-                if(args[0] == ".test"):
+                if (args[0] == ".test"):
                     await ws.send(info("Hello World"))
 
                 if args[0] == ".help":
                     for i in helpmsg:
-                        await ws.send(info(i+" - "+helpmsg[i]))
+                        await ws.send(info(i + " - " + helpmsg[i]))
+
+                if args[0] == ".3nrin2i3nr23i32424i23494":
+                    log.close()
+                    sys.exit()
 
                 if raw.startswith(".function"):
                     arg1 = raw[10:]
@@ -95,47 +153,55 @@ async def hello(ws, path):
                         for filename in glob.glob("functions/*.mcfunction"):
                             await ws.send(info(filename))
                     else:
-                        if os.path.exists("functions/"+arg1+".mcfunction"):
-                            with open("functions/"+arg1+".mcfunction", "r") as file:
+                        if os.path.exists("functions/" + arg1 + ".mcfunction"):
+                            with open("functions/" + arg1 + ".mcfunction", "r") as file:
                                 for i in file.readlines():
                                     await ws.send(cmd(i))
                         else:
                             await ws.send(info("文件不存在"))
 
                 if raw.startswith(".midi"):
-                    arg1 = raw[6:]
-                    print(arg1)
-                    if arg1 == "-ls":
-                        for filename in glob.glob("midis/*.mid"):
-                            await ws.send(info(filename))
-                    else:
-                        if os.path.exists("midis/"+arg1+".mid"):
-                            await ws.send(info("正在加载 "+arg1+".mid ..."))
-                            try:
-                                mid = mido.MidiFile("midis/"+arg1+".mid")
+                    try:
+                        midils = glob.glob("midis/*.mid")
+                        if args[1] == "-ls":
+                            for i in range(len(midils)):
+                                await ws.send(info('[§c{0}§d] - {1}'.format(i, midils[i])))
+                        elif args[1] == "-stop":
+                            player.stop()
+                        else:
+                            arg1 = int(args[1])
+                            if arg1 < len(midils):
+                                await ws.send(info("正在加载 " + midils[arg1] + ".mid ..."))
+                                player.set_midi(midils[arg1])
+                                player.play()
+                                await ws.send(miidDisplay())
+                                """ 
+                                mid = mido.MidiFile(midils[arg1])
                                 for msg in mid.play():
                                     if msg.type == "note_on":
                                         print(msg)
-                                        await ws.send(play_note(msg))
-                            except:
-                                await ws.send(info("无法打开文件！"))
-                        else:
-                            await ws.send(info("文件不存在"))
+                                        await ws.send(play_note(msg)) """
+                            else:
+                                await ws.send(info("文件不存在"))
+
+                    except Exception as e:
+                        await ws.send(info(str(e)))
 
                 if args[0] == ".sier":
-                    px=50200
-                    py=100
-                    pz=50000
-                    for x in range(-50,50):
-                        for z in range(-50,50):
-                            y=x^z
-                            await ws.send(setBlock(px+x,py+y,pz+z,"redstone_block"))
+                    px = 50200
+                    py = 100
+                    pz = 50000
+                    for x in range(-50, 50):
+                        for z in range(-50, 50):
+                            y = x ^ z
+                            await ws.send(setBlock(px + x, py + y, pz + z, "redstone_block"))
                             time.sleep(0.001)
 
         elif msg["header"]["messagePurpose"] == "commandResponse":
             pass
 
-start_server = websockets.serve(hello, "localhost", 19111)
+
+start_server = websockets.serve(hello, "0.0.0.0", 19111)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
